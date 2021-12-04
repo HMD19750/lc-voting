36c1426f1298b49246f37b2abba35ed3b1ce1d0a
hmd <hmd19570@gmail.com>
2021-12-04 23:03:30 +0100

    Aflevering 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+const { colors } = require('tailwindcss/defaultTheme');
 const defaultTheme = require('tailwindcss/defaultTheme');
+
 
 module.exports = {
     purge: [
@@ -9,8 +11,44 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+
+                black: colors.black,
+                white: colors.white,
+                gray: colors.trueGray,
+                'gray-background': '#f7f8fc',
+                'blue': '#328af1',
+                'blue-hover': '#2879bd',
+                'yellow': '#ffc73c',
+                'red': '#ec454f',
+                'red-100': '#fee2e2',
+                'green': '#1aab8b',
+                'green-50': '#f0fdf4',
+                'purple': '#8b60ed',
+            },
+            spacing: {
+                22: '5.5rem',
+                44: '11rem',
+                70: '17.5rem',
+                76: '19rem',
+                104: '26rem',
+                128: '32rem',
+                175: '43.75rem',
+            },
+            maxWidth: {
+                custom: '68.5rem',
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
             },
         },
     },

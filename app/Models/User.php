3c04ed1541725cf89691b93925cae42f674d75e3
hmd <hmd19570@gmail.com>
2021-12-15 16:55:45 +0100

    Aflevering 32 - Set the Ideas Status

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,11 @@ class User extends Authenticatable
             . $integerToUse
             . '.png';
     }
+
+    public function isAdmin()
+    {
+        return in_array($this->email, [
+            'hmd19570@gmail.com'
+        ]);
+    }
 }

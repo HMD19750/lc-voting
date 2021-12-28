d237fa6c7602eb7f47a01741004e0aff0bbdd16e
hmd <hmd19570@gmail.com>
2021-12-28 22:55:05 +0100

    Aflevering 59 - Notification styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,14 +26,114 @@
             @if (Route::has('login'))
             <div class="px-6 py-4">
                 @auth
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+                <div class="flex items-center space-x-4">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
 
-                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                        {{ __('Log out') }}
-                    </a>
-                </form>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                            {{ __('Log out') }}
+                        </a>
+                    </form>
+
+                    <div x-data="{isOpen:false}" class="relative">
+                        <button @click="isOpen=!isOpen">
+                            <svg class="w-8 h-8 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
+                                </path>
+                            </svg>
+                            <div class="absolute flex items-center justify-center w-6 h-6 text-white border-2 rounded-full bg-red text-xxs -top-1 -right-1"
+                                >
+                                2
+                            </div>
+                        </button>
+
+                        <ul
+                            class="absolute z-10 overflow-y-auto text-sm text-left text-gray-700 bg-white -right-28 md:-right-12 w-76 shadow-dialog rounded-xl top-8 md:w-96 max-h-128"
+                            x-show.transition.origin.top="isOpen"
+                            x-cloak
+                            @click.away="isOpen = false"
+                            @keydown.escape.window="isOpen = false"
+                            >
+
+                            <li><a
+                                    href="#"
+                                    @click.prevent="
+                                    isOpen=false
+                                    "
+                                    class="flex px-5 py-3 transition duration-150 ease-in hover:bg-gray-100">
+                                        <img src="http://www.gravatar.com/avatar/?d=mp" class="w-10 h-10 rounded-xl">
+                                        <div class="ml-4">
+                                            <div class="line-clamp-6">
+                                                <span class="font-semibold">Hubert </span>
+                                                commented on
+                                                <span class="font-semibold">This is my idea</span>
+                                                :
+                                                <span>"Occaecat est enim nostrud laboris labore amet dolor cillum reprehenderit dolor laborum deserunt. Exercitation aliquip aute sit officia dolore officia pariatur tempor cupidatat sint deserunt tempor. Aliquip consequat laborum id Lorem Lorem et enim. Elit ipsum nisi nisi in excepteur tempor laborum enim labore anim ex in exercitation. Nisi amet quis sunt proident qui reprehenderit et adipisicing commodo laboris. Do duis eiusmod nulla sit ad elit magna nulla excepteur non. In laboris duis enim est anim exercitation nisi quis eu dolor dolore labore aliqua."</span>
+                                            </div>
+                                            <div class="mt-2 text-xs text-gray-500 ">
+                                                1 hour ago
+                                            </div>
+                                        </div>
+                                </a>
+                            </li>
+                            <li><a
+                                href="#"
+                                @click.prevent="
+                                isOpen=false
+                                "
+                                class="flex px-5 py-3 transition duration-150 ease-in hover:bg-gray-100">
+                                    <img src="http://www.gravatar.com/avatar/?d=mp" class="w-10 h-10 rounded-xl">
+                                    <div class="ml-4">
+                                        <div class="line-clamp-6">
+                                            <span class="font-semibold">Hubert </span>
+                                            commented on
+                                            <span class="font-semibold">This is my idea</span>
+                                            :
+                                            <span>"Occaecat est enim nostrud laboris labore amet dolor cillum reprehenderit dolor laborum deserunt. Exercitation aliquip aute sit officia dolore officia pariatur tempor cupidatat sint deserunt tempor. Aliquip consequat laborum id Lorem Lorem et enim. Elit ipsum nisi nisi in excepteur tempor laborum enim labore anim ex in exercitation. Nisi amet quis sunt proident qui reprehenderit et adipisicing commodo laboris. Do duis eiusmod nulla sit ad elit magna nulla excepteur non. In laboris duis enim est anim exercitation nisi quis eu dolor dolore labore aliqua."</span>
+                                        </div>
+                                        <div class="mt-2 text-xs text-gray-500 ">
+                                            1 hour ago
+                                        </div>
+                                    </div>
+                            </a>
+                        </li>
+                        <li><a
+                            href="#"
+                            @click.prevent="
+                            isOpen=false
+                            "
+                            class="flex px-5 py-3 transition duration-150 ease-in hover:bg-gray-100">
+                                <img src="http://www.gravatar.com/avatar/?d=mp" class="w-10 h-10 rounded-xl">
+                                <div class="ml-4">
+                                    <div class="line-clamp-6">
+                                        <span class="font-semibold">Hubert </span>
+                                        commented on
+                                        <span class="font-semibold">This is my idea</span>
+                                        :
+                                        <span>"Occaecat est enim nostrud laboris labore amet dolor cillum reprehenderit dolor laborum deserunt. Exercitation aliquip aute sit officia dolore officia pariatur tempor cupidatat sint deserunt tempor. Aliquip consequat laborum id Lorem Lorem et enim. Elit ipsum nisi nisi in excepteur tempor laborum enim labore anim ex in exercitation. Nisi amet quis sunt proident qui reprehenderit et adipisicing commodo laboris. Do duis eiusmod nulla sit ad elit magna nulla excepteur non. In laboris duis enim est anim exercitation nisi quis eu dolor dolore labore aliqua."</span>
+                                    </div>
+                                    <div class="mt-2 text-xs text-gray-500 ">
+                                        1 hour ago
+                                    </div>
+                                </div>
+                        </a>
+                    </li>
+
+                    <li class="text-center border-t border-gray-300 ">
+                        <button  class="block w-full px-5 py-4 font-semibold transition duration-150 ease-in focus:font-semibold hover:bg-gray-100">
+                            Mark all as read
+                        </button>
+                    </li>
+
+
+
+
+                        </ul>
+
+                    </div>
+                </div>
                 @else
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
@@ -98,10 +198,7 @@
     </main>
 
     @if(session('success_message'))
-        <x-notification-success
-            :redirect="true"
-            message-to-display="{{ session('success_message') }}"
-            />
+    <x-notification-success :redirect="true" message-to-display="{{ session('success_message') }}" />
     @endif
 
     <livewire:scripts />

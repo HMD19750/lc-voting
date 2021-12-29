34615b0a7f7c2a7e9fd40bb495b688189ccd1f27
hmd <hmd19570@gmail.com>
2021-12-29 21:45:53 +0100

    Aflevering 61 - Badge Count

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,18 +6,33 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFICATION_TRESHOLD = 3;
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
 
     protected $listeners = ['getNotifications'];
 
     public function mount()
     {
         $this->notifications = collect([]);
+        $this->getNotificationCount();
+        $this->isLoading = true;
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+        if ($this->notificationCount > self::NOTIFICATION_TRESHOLD) {
+            $this->notificationCount = self::NOTIFICATION_TRESHOLD . '+';
+        }
     }
 
     public function getNotifications()
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()->user()->unreadNotifications()->latest()->take(self::NOTIFICATION_TRESHOLD)->get();
+
+        $this->isLoading = false;
     }
 
     public function render()
--- resources/views/livewire/comment-notifications.blade.php
+++ resources/views/livewire/comment-notifications.blade.php
@@ -10,18 +10,20 @@
                 d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
             </path>
         </svg>
-        <div
-            class="absolute flex items-center justify-center w-6 h-6 text-white border-2 rounded-full bg-red text-xxs -top-1 -right-1">
-            2
-        </div>
+        @if($notificationCount)
+            <div
+                class="absolute flex items-center justify-center w-6 h-6 text-white border-2 rounded-full bg-red text-xxs -top-1 -right-1">
+                {{ $notificationCount }}
+            </div>
+        @endif
     </button>
 
     <ul class="absolute z-10 overflow-y-auto text-sm text-left text-gray-700 bg-white -right-28 md:-right-12 w-76 shadow-dialog rounded-xl top-8 md:w-96 max-h-128"
         x-show.transition.origin.top="isOpen" x-cloak @click.away="isOpen = false"
         @keydown.escape.window="isOpen = false">
 
-        @foreach ($notifications as $notification )
-
+        @if($notifications->isNotEmpty() && !$isLoading)
+        @foreach($notifications as $notification )
 
         <li><a href="{{ route('idea.show',$notification->data['idea_slug']) }}"
             {{-- @click.prevent="isOpen=false"  --}}
@@ -43,13 +45,31 @@
         </li>
         @endforeach
 
+
         <li class="text-center border-t border-gray-300 ">
             <button
                 class="block w-full px-5 py-4 font-semibold transition duration-150 ease-in focus:font-semibold hover:bg-gray-100">
                 Mark all as read
             </button>
         </li>
+        @elseif($isLoading)
+        @foreach(range(1,3) as $item)
+            <li class="flex px-5 py-3 transition duration-150 ease-in animate-pulse">
+                <div class="w-10 h-10 bg-gray-200 rounded-xl"></div>
+                <div class="flex-1 ml-4 space-y-1">
+                    <div class="w-full h-4 bg-gray-200 rounded "></div>
+                    <div class="w-full h-4 bg-gray-200 rounded "></div>
+                    <div class="w-full h-4 bg-gray-200 rounded "></div>
+                    <div class="w-1/2 h-3 bg-gray-200 rounded-xl "></div>
+                </div>
+            </li>
+        @endforeach
 
+        @else
+        <li class="w-40 py-6 mx-auto">
+            <p class="mt-6 mb-4 font-bold text-center text-gray-400">No new notifications.</p>
+        </li>
+        @endif
 
 
 
